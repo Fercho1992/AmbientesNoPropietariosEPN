4d2c14f02cc5886a39493b419d3fcc16fbb219bb
Fercho1992 <fernandoalex_92@hotmail.com>
2015-04-28 19:00:46 -0500

    practicaClase

---
+++ practicaClase/index.php
@@ -0,0 +1,17 @@
+<?php
+
+echo 'hola'.'mundo';
+echo '<br>';
+echo 'hola'+'mundo';
+echo '<br>';
+echo 4 + 'mundo';
+echo '<br>';
+echo 'mundo' + 4;
+echo '<br>';
+
+echo 4 + 'mundo' . 4;
+
+
+
+?>
+
