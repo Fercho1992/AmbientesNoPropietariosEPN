b9a79b4ee2c5f096ad671440fa44356480201847
Fercho1992 <fernandoalex_92@hotmail.com>
2015-04-28 19:43:06 -0500

    practicaClase

--- practicaClase/index.php
+++ practicaClase/index.php
@@ -10,7 +10,16 @@ echo 'mundo' + 4;
 echo '<br>';
 
 echo 4 + 'mundo' . 4;
-
+echo '<br>';
+echo $valor = 1*2*3/4*5;
+echo '<br>';
+echo "Precedencia de operadores";
+echo '<br>';
+echo $valor1 = ((1+2)*3-4)*5;
+echo '<br>';
+echo $valor2 = (2-4)*(5*3)+1;
+echo '<br>';
+echo $valor3 = (5+2-4+1)*3;
 
 
 ?>
