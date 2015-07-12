7d3d3e1e32fe97af48799e189fa220c673e2eb02
FernandoMoya <fernandoalexander2014@gmail.com>
2015-07-12 18:14:28 -0500

    Sesion 14 Facebook SDK V4 en PHP

---
+++ deberes/facebook/config/facebook.php
@@ -0,0 +1,15 @@
+<?php
+$config = array(
+	'app_id' =>'1443381725971499' ,
+'app_secret'=>'901f24047cbdd8af6d720150c8cdfa6a',
+'scopes'=> array('email','read_friendlists','user_online_presence')
+);
+
+
+/*$fb = new Facebook\Facebook([
+  'app_id' => '1443381725971499',
+  'app_secret' => '901f24047cbdd8af6d720150c8cdfa6a',
+  'default_graph_version' => 'v2.4',
+  ]);*/
+
+  ?>
